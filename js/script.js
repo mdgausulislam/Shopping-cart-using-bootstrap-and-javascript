9467b2593bdbc3ba590f6465edb992867b09f8a1
unknown <gausulislam5@gmail.com>
2023-09-19 17:38:58 +0600

    update section

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 document.getElementById('btn-plus').addEventListener('click',function(){
-    console.log('click plus');
+    // console.log('click plus');
     const previousInputItems=inputFieldValueById('input-field');
-
     const newInputItems= previousInputItems+1;
     setTextInputById('input-field',newInputItems);
+    const newTotalPrice=newInputItems * 1219;
+    const newTotalPriceElements=document.getElementById('total-price');
+    newTotalPriceElements.innerText=newTotalPrice;
 
     // const inputField=document.getElementById('input-field');
     //     const newInputItemsString=inputField.value;
@@ -13,5 +15,8 @@ document.getElementById('btn-plus').addEventListener('click',function(){
 })
 
 document.getElementById('btn-minus').addEventListener('click',function(){
-    
+    const previousInputItems=inputFieldValueById('input-field');
+    const newInputItems= previousInputItems-1;
+    setTextInputById('input-field',newInputItems);
+   
 })
